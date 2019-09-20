d15da74a0051b278d878c3e7c3a4ac1e4877a0a8
Eran <erantzur1@gmail.com>
2019-09-20 10:25:14 +0300

    Create resources

---
+++ app/Post.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $guarded = [];
+
+    public function user(){
+        $this->belongsTo(User::class);
+    }
+}
---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Post;
+use App\User;
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class, 50)->create()->each(function ($user) {
+            $user->posts()->saveMany(factory(Post::class, 5)->make());
+        });
+    }
+}
